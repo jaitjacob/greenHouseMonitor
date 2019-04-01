e9f7f96afa0f3d73e155aafab1bd20c47340dc3e
Jait <jai8.jacob@gmail.com>
2019-04-01 19:06:50 +1100

    create table has to run only once

--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -39,5 +39,5 @@ if __name__ == "__main__":
     #createDB()
     ##getSensorData()
     monitorObj = monitor()
-    monitorObj.createDB()
+    #monitorObj.createDB()
     monitorObj.getSensorData()
