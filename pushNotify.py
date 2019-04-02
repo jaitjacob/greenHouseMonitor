1c45e746a4d92eec227d9f9e24a6641e42d3e23c
Jait <jai8.jacob@gmail.com>
2019-04-02 20:36:44 +1100

    minor modifications

--- pushNotify.py
+++ pushNotify.py
@@ -1,6 +1,6 @@
 import requests,json,os,sqlite3
 
-def send_notification_via_pushbullet(self, title, body):
+def send_notification_via_pushbullet(title, body):
         ACCESS_TOKEN = "o.Uku0RMLmpUV18bnGkAgkpYQB2mGzyAko"
         """ Sending notification via pushbullet.
             Args:
@@ -8,13 +8,7 @@ def send_notification_via_pushbullet(self, title, body):
                 body (str) : Body of text.
         """
         data = { "type": "note", "title": title, "body": body }
-
         response = requests.post("https://api.pushbullet.com/v2/pushes", data = json.dumps(data),
             headers = { "Authorization": "Bearer " + ACCESS_TOKEN, "Content-Type": "application/json" })
-
         if(response.status_code != 200):
             raise Exception()
-
-        print("Notification sent.")
-
-send_notification_via_pushbullet("this title","ENDGAME is coming")
