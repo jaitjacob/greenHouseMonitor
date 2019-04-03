7445b1fbb3e9191b0c3fd22f2f2bd1ebc8c2fcd7
Jait <jai8.jacob@gmail.com>
2019-04-03 23:41:02 +1100

    some more code

--- configFetcher.py
+++ configFetcher.py
@@ -6,3 +6,15 @@ with open("config.json", "r") as file:
 def getMinTemperature():
     _ = data[0]['min_temperature']
     return _
+
+def getMaxTemperature():
+    _ = data[0]['max_temperature']
+    return _
+
+def getMinHumidity():
+    _ = data[0]['min_humidity']
+    return _
+        
+def getMaxHumidity():
+    _ = data[0]['max_humidity']
+    return _
