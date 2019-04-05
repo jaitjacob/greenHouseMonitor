4b6fa98248c2ab060355c1ad22e170c6fbfdc6d8
Jait <jai8.jacob@gmail.com>
2019-04-05 15:39:46 +1100

    minor OO aspect modification

--- createReport.py
+++ createReport.py
@@ -12,62 +12,64 @@ ONE_DAY_DELTA = timedelta(days = 1)
 
 class Reporter:
 
-def generateReportString(self, recordedTempMax, recordedTempMin, recordedHumMax, recordedHumMin, maxTemp, maxHum, minTemp, minHum, currentDate):
-    with open("report.csv", "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        message=""
-        if(recordedTempMax>maxTemp):
-                percent = (recordedTempMax/maxTemp)*100
-                message = " current temperature exceeds configured temperature by." + str(percent) + "%."
+        def generateReportString(self, recordedTempMax, recordedTempMin, recordedHumMax, recordedHumMin, maxTemp, maxHum, minTemp, minHum, currentDate):
+                with open("report.csv", "w", newline="") as csvfile:
+                        writer = csv.writer(csvfile)
+                        message=""
+                        if(recordedTempMax>maxTemp):
+                                percent = (recordedTempMax/maxTemp)*100
+                                message = " current temperature exceeds configured temperature by " + str(percent) + "%."
 
-        if(recordedTempMin<minTemp):
-                percent = (recordedTempMin/minTemp)*100
-                message = message+"current temperature is"+str(percent)+ "% below configured temperature. "
+                        if(recordedTempMin<minTemp):
+                                percent = (recordedTempMin/minTemp)*100
+                                message = message+"current temperature is"+str(percent)+ "% below configured temperature. "
 
-        if(recordedHumMax>maxHum):
-                percent = (recordedHumMax/maxHum)*100
-                message = message+"current humidity exceed configured humidity. "
+                        if(recordedHumMax>maxHum):
+                                percent = (recordedHumMax/maxHum)*100
+                                message = message+"current humidity exceed configured humidity by " + + str(percent) + "%."
 
-        if(recordedHumMin<minHum):
-                percent = (recordedHumMin/minHum)*100
-                message = message+"current humidity is below configured humidity. "
+                        if(recordedHumMin<minHum):
+                                percent = (recordedHumMin/minHum)*100
+                                message = message+"current humidity is "+str(percent)+ "% below configured humidity. "
 
-        writer.writerow([currentDate,message])
+                        writer.writerow([currentDate,message])
 
-def generateReport():
-    connection = sqlite3.connect(DB_NAME)
-    connection.row_factory = sqlite3.Row
-   
-    with connection:
-        cursor = connection.cursor()
-        row = cursor.execute("SELECT DATE(MIN(date)), DATE(MAX(date)) FROM sensor").fetchone()
-        startDate = datetime.strptime(row[0], DATE_FORMAT)
-        endDate = datetime.strptime(row[1], DATE_FORMAT)
 
-        print("Dates:")
-        date = startDate
-
-        while date <= endDate:
-            row = cursor.execute(
-                """SELECT COUNT(*), max(temperature), min(temperature), max(humidity), min(humidity) FROM sensor
-                WHERE date >= DATE(:date) AND date < DATE(:date, '+1 day')""",
-                { "date": date.strftime(DATE_FORMAT) }).fetchone()
+        def generateReport(self):
+                connection = sqlite3.connect(DB_NAME)
+                connection.row_factory = sqlite3.Row
                 
-            print(date.strftime(DATE_FORMAT) + " | Readings recorded: " + str(row[0]) + "\nMax Temperature: " + str(row[1]) + "\nMin Temperature: " + str(row[2]) + "\nMax Humidity: " + str(row[3]) + "\nMin Humidity: " + str(row[4]) )
-            recordedTempMax = row[1]
-            recordedTempMin = row[2]
-            recordedHumMax = row[3]
-            recordedHumMin = row[4]
-            maxTemp = configFetcher.getMaxTemperature()
-            minTemp = configFetcher.getMinTemperature()
-            maxHum = configFetcher.getMaxHumidity()
-            minHum = configFetcher.getMinHumidity()
-            currentDate=date.strftime(DATE_FORMAT)
+                with connection:
+                        cursor = connection.cursor()
+                        row = cursor.execute("SELECT DATE(MIN(date)), DATE(MAX(date)) FROM sensor").fetchone()
+                        startDate = datetime.strptime(row[0], DATE_FORMAT)
+                        endDate = datetime.strptime(row[1], DATE_FORMAT)
 
-            self.generateReportString(recordedTempMax, recordedTempMin, recordedHumMax, recordedHumMin, maxTemp, maxHum, minTemp, minHum, currentDate)
-            date += ONE_DAY_DELTA
-    connection.close()
+                        print("Dates:")
+                        date = startDate
+
+                        while date <= endDate:
+                                row = cursor.execute(
+                                        """SELECT COUNT(*), max(temperature), min(temperature), max(humidity), min(humidity) FROM sensor
+                                        WHERE date >= DATE(:date) AND date < DATE(:date, '+1 day')""",
+                                        { "date": date.strftime(DATE_FORMAT) }).fetchone()
+                                
+                        print(date.strftime(DATE_FORMAT) + " | Readings recorded: " + str(row[0]) + "\nMax Temperature: " + str(row[1]) + "\nMin Temperature: " + str(row[2]) + "\nMax Humidity: " + str(row[3]) + "\nMin Humidity: " + str(row[4]) )
+                        recordedTempMax = row[1]
+                        recordedTempMin = row[2]
+                        recordedHumMax = row[3]
+                        recordedHumMin = row[4]
+                        maxTemp = configFetcher.getMaxTemperature()
+                        minTemp = configFetcher.getMinTemperature()
+                        maxHum = configFetcher.getMaxHumidity()
+                        minHum = configFetcher.getMinHumidity()
+                        currentDate=date.strftime(DATE_FORMAT)
+
+                        self.generateReportString(recordedTempMax, recordedTempMin, recordedHumMax, recordedHumMin, maxTemp, maxHum, minTemp, minHum, currentDate)
+                        date += ONE_DAY_DELTA
+                connection.close()
 
 # Execute program.
 if __name__ == "__main__":
-    main()
+    report = Reporter()
+    report.generateReport()
