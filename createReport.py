a2b1fd36802f85495d1901b38739da1c817382be
Jait <jai8.jacob@gmail.com>
2019-04-05 16:06:06 +1100

    minor fix

--- createReport.py
+++ createReport.py
@@ -13,8 +13,6 @@ ONE_DAY_DELTA = timedelta(days = 1)
 class Reporter:
 
         def generateReportString(self, recordedTempMax, recordedTempMin, recordedHumMax, recordedHumMin, maxTemp, maxHum, minTemp, minHum, currentDate):
-                with open("report.csv", "w", newline="") as csvfile:
-                        writer = csv.writer(csvfile)
                         message=""
                         if(recordedTempMax>maxTemp):
                                 percent = (recordedTempMax/maxTemp)*100
@@ -31,9 +29,7 @@ class Reporter:
                         if(recordedHumMin<minHum):
                                 percent = (recordedHumMin/minHum)*100
                                 message = message+"current humidity is "+str(percent)+ "% below configured humidity. "
-
-                        writer.writerow([currentDate,message])
-
+                        return message
 
         def generateReport(self):
                 connection = sqlite3.connect(DB_NAME)
@@ -48,25 +44,28 @@ class Reporter:
                         print("Dates:")
                         date = startDate
 
-                        while date <= endDate:
-                                row = cursor.execute(
-                                        """SELECT COUNT(*), max(temperature), min(temperature), max(humidity), min(humidity) FROM sensor
-                                        WHERE date >= DATE(:date) AND date < DATE(:date, '+1 day')""",
-                                        { "date": date.strftime(DATE_FORMAT) }).fetchone()
-                                
-                                print(date.strftime(DATE_FORMAT) + " | Readings recorded: " + str(row[0]) + "\nMax Temperature: " + str(row[1]) + "\nMin Temperature: " + str(row[2]) + "\nMax Humidity: " + str(row[3]) + "\nMin Humidity: " + str(row[4]) )
-                                recordedTempMax = row[1]
-                                recordedTempMin = row[2]
-                                recordedHumMax = row[3]
-                                recordedHumMin = row[4]
-                                maxTemp = configFetcher.getMaxTemperature()
-                                minTemp = configFetcher.getMinTemperature()
-                                maxHum = configFetcher.getMaxHumidity()
-                                minHum = configFetcher.getMinHumidity()
-                                currentDate=date.strftime(DATE_FORMAT)
+                        with open("report.csv", "w", newline="") as csvfile:
+                                writer = csv.writer(csvfile)
+                                while date <= endDate:
+                                        row = cursor.execute(
+                                                """SELECT COUNT(*), max(temperature), min(temperature), max(humidity), min(humidity) FROM sensor
+                                                WHERE date >= DATE(:date) AND date < DATE(:date, '+1 day')""",
+                                                { "date": date.strftime(DATE_FORMAT) }).fetchone()
+                                        
+                                        print(date.strftime(DATE_FORMAT) + " | Readings recorded: " + str(row[0]) + "\nMax Temperature: " + str(row[1]) + "\nMin Temperature: " + str(row[2]) + "\nMax Humidity: " + str(row[3]) + "\nMin Humidity: " + str(row[4]) )
+                                        recordedTempMax = row[1]
+                                        recordedTempMin = row[2]
+                                        recordedHumMax = row[3]
+                                        recordedHumMin = row[4]
+                                        maxTemp = configFetcher.getMaxTemperature()
+                                        minTemp = configFetcher.getMinTemperature()
+                                        maxHum = configFetcher.getMaxHumidity()
+                                        minHum = configFetcher.getMinHumidity()
+                                        currentDate=date.strftime(DATE_FORMAT)
 
-                                self.generateReportString(recordedTempMax, recordedTempMin, recordedHumMax, recordedHumMin, maxTemp, maxHum, minTemp, minHum, currentDate)
-                                date += ONE_DAY_DELTA
+                                        reportString = self.generateReportString(recordedTempMax, recordedTempMin, recordedHumMax, recordedHumMin, maxTemp, maxHum, minTemp, minHum, currentDate)
+                                        writer.writerow([currentDate,reportString])
+                                        date += ONE_DAY_DELTA
                 connection.close()
 
 # Execute program.
