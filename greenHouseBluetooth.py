38252c583faded1c588beea2b4ffed7d0ee96604
Jait <jai8.jacob@gmail.com>
2019-04-07 02:48:32 +1000

    minor fix

--- greenHouseBluetooth.py
+++ greenHouseBluetooth.py
@@ -1,38 +1,42 @@
-import bluetooth, time, os
+import bluetooth
+import time
+import os
 import subprocess as sp
+
 
 class bluetoothNotify:
     def findNearByDevices(self):
-        while time.sleep(5):
-            print("Scanning...")
-            nearbyDevices = bluetooth.discover_devices()
+        print("Scanning...")
+        nearbyDevices = bluetooth.discover_devices()
 
-            for macAddress in nearbyDevices:
-                print("Found device with mac-address: " + macAddress)
+        for macAddress in nearbyDevices:
+            print("Found device with mac-address: " + macAddress)
 
-    def sliceMacAddress(self,macAddress):
+    def sliceMacAddress(self, macAddress):
         sliceStart = int(macAddress.rindex("("))
         sliceEnd = int(macAddress.rindex(")"))
-        _=""
-        
-        for x in range(sliceStart+1,sliceEnd):
-            _=_+macAddress[x]            
+        _ = ""
+
+        for x in range(sliceStart+1, sliceEnd):
+            _ = _+macAddress[x]
 
         return macAddress
 #
 
-    
     def listPairedDevices(self):
-        p = sp.Popen(["bt-device", "--list"], stdin = sp.PIPE, stdout = sp.PIPE, close_fds = True)
+        p = sp.Popen(["bt-device", "--list"], stdin=sp.PIPE,
+                     stdout=sp.PIPE, close_fds=True)
         (stdout, stdin) = (p.stdout, p.stdin)
         data = stdout.readlines()
-        myphone=data.pop(1)
-        myphone=myphone.decode()
+        myphone = data.pop(1)
+        myphone = myphone.decode()
         macAddress = self.sliceMacAddress(myphone)
         return macAddress
 
 
 if __name__ == "__main__":
     blue = bluetoothNotify()
+    blue.findNearByDevices()
+    blue.findNearByDevices()
     blue.findNearByDevices()
     blue.listPairedDevices()
